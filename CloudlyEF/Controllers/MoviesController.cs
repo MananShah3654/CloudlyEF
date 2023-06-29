8200647c45f0ffc3bf0b06db924ad7ec928fcc35
Manan Shah <mananshah3654@gmail.com>
2023-06-29 14:21:07 +0530

    MVC in Action

---
+++ CloudlyEF/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using CloudlyEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CloudlyEF.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new Movies() { Name = "Adipurush !" };
+            return View(movies);
+        }
+    }
+}
