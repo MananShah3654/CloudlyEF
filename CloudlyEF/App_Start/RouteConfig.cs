b478febf5050b0149ca1edd37534439fc55cbe3e
Manan Shah <mananshah3654@gmail.com>
2023-06-29 14:25:30 +0530

    Action Result, Action Parameter , Conventional and Attribute routing.

--- CloudlyEF/App_Start/RouteConfig.cs
+++ CloudlyEF/App_Start/RouteConfig.cs
@@ -12,7 +12,17 @@ namespace CloudlyEF
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            // Example of Conventional Routing
 
+            //routes.MapRoute(
+            //    "MovieReleaseByDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "movies", action = "ByReleaseDate" }
+            //    );
+
+            // Example of Attribute Routing
+
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- CloudlyEF/Controllers/MoviesController.cs
+++ CloudlyEF/Controllers/MoviesController.cs
@@ -13,7 +13,47 @@ namespace CloudlyEF.Controllers
         public ActionResult Index()
         {
             var movies = new Movies() { Name = "Adipurush !" };
+
+            //* Different types of Action Results
+
+            //return new ViewResult();
+            //return Content("Hello Word!");
+            //return HttpNotFound();
+            //return RedirectToAction("Index", "Home");
             return View(movies);
         }
+
+        #region Action Parameters
+
+        public ActionResult edit(int movieId)
+        {
+            return Content("id = " + movieId);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        #endregion
+
+        #region Conventional & Attribute Routing
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        #endregion
     }
 }
