ccbb8b4be7050e47f0be2abb6f10e0d8a8eeb853
Manan Shah <mananshah3654@gmail.com>
2023-07-04 18:01:12 +0530

    Add a form to add/update a customer.

--- CloudlyEF/Controllers/CustomerController.cs
+++ CloudlyEF/Controllers/CustomerController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using CloudlyEF.ViewModel;
 
 namespace CloudlyEF.Controllers
 {
@@ -29,7 +30,47 @@ namespace CloudlyEF.Controllers
             return View(customers);
         }
 
-        public ActionResult Details(int id)
+       public ActionResult New()
+        {
+            var membershipType = _context.MembershipTypes.ToList();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                MembeshipType = membershipType
+            };
+            
+            return View("CustomerForm",viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Customers customer)
+        {
+            if(customer.Id == 0)
+            _context.Customers.Add(customer);
+            else{
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedNewsLetter = customer.IsSubscribedNewsLetter;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index","Customer");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembeshipType = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+            public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
---
+++ CloudlyEF/ViewModel/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CloudlyEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudlyEF.ViewModel
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembeshipType { get; set; }
+        public Customers Customer { get; set; }
+    }
+}
